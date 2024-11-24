074b5b9754c6640c3dd0d669f0537958fa938c02
Alfio Martini <alfiomartini@velocityglobal.com>
2024-11-24 10:25:20 -0300

    chore: add esbuild configuration

---
+++ esbuild.config.js
@@ -0,0 +1,19 @@
+// esbuild.config.js
+const esbuild = require("esbuild");
+
+esbuild
+  .build({
+    entryPoints: [
+      "src/handlers/GetBook/getBook.ts",
+      "src/handlers/UpdateBook/updateBook.ts",
+      "src/handlers/CreateBook/createBook.ts",
+    ],
+    bundle: true,
+    platform: "node",
+    target: "node20",
+    outdir: "dist/src",
+    external: ["aws-sdk"],
+    minify: true,
+    sourcemap: true,
+  })
+  .catch(() => process.exit(1));
